bcbb71908d80e5cb9d29b3fe1f092b1b99268f31
Karthik Shiva <karthik.shiva@zohocorp.com>
2021-11-10 21:43:55 +0530

    first commit

---
+++ Shared/SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleApp.swift
+//  Shared
+//
+//  Created by karthik-6762 on 25/08/21.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
