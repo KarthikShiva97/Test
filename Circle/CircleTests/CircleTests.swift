24bc695bfb33d6947620140c9cb68190e7f12a33
Karthik Shiva <karthik.shiva@zohocorp.com>
2021-11-21 20:36:44 +0530

    added files

---
+++ Circle/CircleTests/CircleTests.swift
@@ -0,0 +1,32 @@
+//
+//  CircleTests.swift
+//  CircleTests
+//
+//  Created by karthik-6762 on 21/11/21.
+//
+
+import XCTest
+@testable import Circle
+
+class CircleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        XCTAssert(1 == 1)
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
